7761786225ed1419de0c8b6b73d540639ea829b1
Bushra <77388684+bushra1997@users.noreply.github.com>
2021-05-16 12:57:58 +0300

    practice getAll And create

--- app.js
+++ app.js
@@ -5,8 +5,35 @@ const db = require("./db");
 const app = express();
 app.use(express.json());
 
-app.get("/todos", (req, res) => {});
-app.post("/create/todo", (req, res) => {});
+app.get("/todos", (req, res) => {
+  todoModel
+    .find({})
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
+app.post("/create/todo", (req, res) => {
+  const { task, description, deadline, isCompleted, priority } = req.body;
+  const todo = new todoModel({
+    task,
+    description,
+    deadline,
+    isCompleted,
+    priority,
+  });
+
+  todo
+    .save()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 app.put("/update/todo", (req, res) => {});
 app.delete("/delete/todo", (req, res) => {});
 
